2110aed975dd192b787bfb0fca522ee9e2fee64f
MarkChang <markchang@kkbox.com>
2017-02-14 22:29:10 +0800

    Tunes code.

--- Lyrics/LyricsTests/LyricsParserTests.m
+++ Lyrics/LyricsTests/LyricsParserTests.m
@@ -1,22 +1,21 @@
 #import <XCTest/XCTest.h>
 #import "MKLyricsParser.h"
 
-@interface LyricsTests : XCTestCase
+@interface LyricsParserTests : XCTestCase
 
 @end
 
-@implementation LyricsTests
+@implementation LyricsParserTests
 
 MKLyricsParser *lyricsParser;
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
 	lyricsParser = [[MKLyricsParser alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
