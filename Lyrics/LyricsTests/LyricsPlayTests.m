3835ea6c4ea8974e45df069afc76eef5fb56caa1
MarkChang <markchang@kkbox.com>
2017-02-14 22:47:18 +0800

    Tunes test code.

--- Lyrics/LyricsTests/LyricsPlayTests.m
+++ Lyrics/LyricsTests/LyricsPlayTests.m
@@ -11,12 +11,11 @@ MKLyricsViewController *mkLyricsViewController;
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
 	mkLyricsViewController = [[MKLyricsViewController alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
